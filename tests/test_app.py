51c1099cfe6b12e205d3eb5a9c36b210c894ac4f
aimennabi <aimen.nabi@etu.univ-amu.fr>
2025-02-20 18:41:25 +0000

    resolve Ruff linting issues in test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,14 @@
 import json
-import os
+from pathlib import Path
 
 from lambda_app.app import lambda_handler
 
+
 def load_event(file_name):
-    with open(os.path.join('events', file_name), 'r') as file:
+    event_path = Path('events') / file_name
+    with event_path.open() as file:
         return json.load(file)
+
 
 def test_lambda_handler_success():
     event = load_event('event.json')
@@ -16,6 +19,7 @@ def test_lambda_handler_success():
     assert response['statusCode'] == 200
     assert body['result'] == "The received message is: 'hello world'"
 
+
 def test_lambda_handler_missing_body():
     event = load_event('event_missing_body.json')
     context = {}
@@ -25,6 +29,7 @@ def test_lambda_handler_missing_body():
     assert response['statusCode'] == 400
     assert body['error'] == "Missing 'body' in event."
 
+
 def test_lambda_handler_invalid_json():
     event = load_event('event_no_message.json')
     context = {}
@@ -33,3 +38,4 @@ def test_lambda_handler_invalid_json():
 
     assert response['statusCode'] == 400
     assert body['error'] == "Missing 'message' key in request body."
+
